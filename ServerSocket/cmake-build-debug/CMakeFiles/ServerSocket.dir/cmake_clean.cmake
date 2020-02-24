2e42d9822f8a0d994cb14fbb7bb0d266ab056e88
dasome29 <dasome29@hotmail.com>
2020-02-23 18:20:23 -0600

    Basic server and client interaction.

---
+++ ServerSocket/cmake-build-debug/CMakeFiles/ServerSocket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerSocket.dir/main.cpp.o"
+  "ServerSocket"
+  "ServerSocket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerSocket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
