53fe6f656c891efcaef2ac31b01cdb84acfc3039
dasome29 <dasome29@hotmail.com>
2020-02-29 23:32:26 -0600

    LinkedList.hpp added

--- ServerSocket/LinkedList.hpp
+++ ServerSocket/LinkedList.hpp
@@ -4,5 +4,109 @@
 
 #ifndef SERVERSOCKET_LINKEDLIST_HPP
 #define SERVERSOCKET_LINKEDLIST_HPP
+#define NULL __null
+#include <iostream>
+using namespace std;
+
+template <typename T> class Node {
+public:
+    T data;
+    Node *prev;
+    Node *next;
+
+    Node() {
+        data = NULL;
+        next = NULL;
+        prev = NULL;
+    }
+
+    Node(T d) {
+        data = d;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+
+template <typename B> class LinkedList {
+public:
+    Node<B> *first;
+    Node<B> *last;
+
+    LinkedList<B>() {
+        first = NULL;
+        last = NULL;
+    }
+
+    LinkedList<B>(Node<B> *node) {
+        first = node;
+    }
+
+    // template <typename B>
+    void addFirst(B data) {
+        Node<B> *new_node = new Node<B>(data);
+
+        if (first != NULL) {
+            new_node->next = first;
+            first->prev = new_node;
+        }
+        if (first == NULL) {
+            last = new_node;
+        }
+        first = new_node;
+    }
+
+    void addLast(B data) {
+        Node<B> *new_node = new Node<B>(data);
+        if (last == NULL) {
+            first = new_node;
+            last = new_node;
+            first->next = last;
+            last->prev = first;
+
+        } else {
+            last->next = new_node;
+            new_node->prev = last;
+            last = new_node;
+        }
+
+    }
+
+    void insert(int position, B data) {
+        Node<B>  *new_node = new Node<B>(data);
+
+        Node<B> *temp = first;
+        int i = 0;
+        while (temp != NULL){
+            if (i == position){
+                if (temp == first){
+                    first = new_node;
+                } else{
+                    temp->prev->next = new_node;
+                }
+
+                new_node->next = temp;
+                new_node->prev = temp->prev;
+                temp->prev = new_node;
+                temp = NULL;
+            }else {
+                i++;
+                temp = temp->next;
+            }
+        }
+
+    }
+
+    void print() {
+        Node<B> *current = first;
+
+        while (current != NULL) {
+            int num = current->data;
+            cout << num << endl;
+            current = current->next;
+        }
+    }
+};
+
 
 #endif //SERVERSOCKET_LINKEDLIST_HPP
--- ServerSocket/main.cpp
+++ ServerSocket/main.cpp
@@ -6,35 +6,39 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <string>
+#include "LinkedList.hpp"
 
 using namespace std;
 
-int main()
-{
+class Graph{
+
+};
+
+int main() {
     // Create a socket
     int listening = socket(AF_INET, SOCK_STREAM, 0);
-    if (listening == -1)
-    {
+    if (listening == -1) {
         cerr << "Can't create a socket! Quitting" << endl;
         return -1;
     }
 
     // Bind the ip address and port to a socket
-    sockaddr_in hint;
+    sockaddr_in hint{};
     hint.sin_family = AF_INET;
     hint.sin_port = htons(54000);
     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
-    bind(listening, (sockaddr*)&hint, sizeof(hint));
+    bind(listening, (sockaddr *) &hint, sizeof(hint));
 
     // Tell Winsock the socket is for listening
     listen(listening, SOMAXCONN);
 
     // Wait for a connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
+    sockaddr_in client{};
+    socklen_t clientSize;
+    clientSize = sizeof(client);
 
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+    int clientSocket = accept(listening, (sockaddr *) &client, &clientSize);
 
     char host[NI_MAXHOST];      // Client's remote name
     char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
@@ -42,12 +46,9 @@ int main()
     memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
     memset(service, 0, NI_MAXSERV);
 
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-    {
+    if (getnameinfo((sockaddr *) &client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
         cout << host << " connected on port " << service << endl;
-    }
-    else
-    {
+    } else {
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
         cout << host << " connected on port " << ntohs(client.sin_port) << endl;
     }
@@ -58,24 +59,24 @@ int main()
     // While loop: accept and echo message back to client
     char buf[4096];
 
-    while (true)
-    {
+    while (true) {
         memset(buf, 0, 4096);
 
         // Wait for client to send data
-        int bytesReceived = recv(clientSocket, buf, 4096, 0);
-        if (bytesReceived == -1)
-        {
+        int bytesReceived;
+        bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+        if (bytesReceived == -1) {
             cerr << "Error in recv(). Quitting" << endl;
             break;
         }
 
-        if (bytesReceived == 0)
-        {
+        if (bytesReceived == 0) {
             cout << "Client disconnected " << endl;
             break;
         }
 
+        // Receive the message and
         cout << string(buf, 0, bytesReceived) << endl;
 
         // Echo message back to client
@@ -86,4 +87,6 @@ int main()
     close(clientSocket);
 
     return 0;
+
+
 }
